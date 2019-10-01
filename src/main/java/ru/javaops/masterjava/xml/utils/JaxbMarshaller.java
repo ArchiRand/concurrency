5caf3ada4066c46decfc1ed60909c89ad9a02ab4
archirand <archirand@gmail.com>
2019-10-01 19:53:03 +0300

    Добавил утильные классы для marshall unmarshall

---
+++ src/main/java/ru/javaops/masterjava/xml/utils/JaxbMarshaller.java
@@ -0,0 +1,46 @@
+package ru.javaops.masterjava.xml.utils;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
+import javax.xml.validation.Schema;
+import java.io.StringWriter;
+import java.io.Writer;
+
+public class JaxbMarshaller {
+
+    private Marshaller marshaller;
+
+    public JaxbMarshaller(JAXBContext ctx) throws JAXBException {
+        this.marshaller = ctx.createMarshaller();
+        // Свойство, указывающие необходимо ли при мапинге в XML соблюдать выравнивание и отступы
+        this.marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        this.marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+        // Иногда требуется вывести XML без заголовка <xml version=»1.0″ encoding=»UTF-8″ standalone=»yes»?>
+        // Чтобы сделать это нужно маршаллеру указать свойство JAXB_FRAGMENT = true
+        this.marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
+    }
+
+    // Для установки дополнительных свойств маршаллеру
+    public void setProperty(String prop, Object value) throws PropertyException {
+        marshaller.setProperty(prop, value);
+    }
+
+    // Schema - это механизм валидации для документа, который будет сгенерирован
+    // Если в метод передать null это выключит валидацию
+    public synchronized void setSchema(Schema schema) {
+        marshaller.setSchema(schema);
+    }
+
+    //
+    public String marshal(Object instance) throws JAXBException {
+        StringWriter sw = new StringWriter();
+        marshal(instance, sw);
+        return sw.toString();
+    }
+
+    public synchronized void marshal(Object instance, Writer writer) throws JAXBException {
+        marshaller.marshal(instance, writer);
+    }
+}
