2454acf6f6722aabb89ace743baedf6cc4af816c
archirand <archirand@gmail.com>
2019-11-02 20:23:04 +0300

    Разбил проект на модули

---
+++ test/src/test/ru/javaops/masterjava/xml/utils/JaxbParserTest.java
@@ -0,0 +1,54 @@
+package ru.javaops.masterjava.xml.utils;
+
+import com.google.common.io.Resources;
+import org.junit.Assert;
+import org.junit.Test;
+import ru.javaops.masterjava.xml.MainXML;
+import ru.javaops.masterjava.xml.schema.CityType;
+import ru.javaops.masterjava.xml.schema.ObjectFactory;
+import ru.javaops.masterjava.xml.schema.Payload;
+import ru.javaops.masterjava.xml.schema.User;
+import ru.javaops.masterjava.xml.utils.jaxb.JaxbParser;
+import ru.javaops.masterjava.xml.utils.jaxb.Schemas;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+import java.util.List;
+
+public class JaxbParserTest {
+
+    private static final JaxbParser JAXB_PARSER = new JaxbParser(ObjectFactory.class);
+
+    static {
+        JAXB_PARSER.setSchema(Schemas.ofClasspath("src/main/resources/payload.xsd"));
+    }
+
+    @Test
+    public void testPayload() throws Exception {
+//        JaxbParserTest.class.getResourceAsStream("/city.xml")
+        Payload payload = JAXB_PARSER.unmarshal(
+                Resources.getResource("src/main/resources/payload.xml").openStream());
+        String strPayload = JAXB_PARSER.marshal(payload);
+        JAXB_PARSER.validate(strPayload);
+        System.out.println(strPayload);
+    }
+
+    @Test
+    public void testCity() throws Exception {
+        JAXBElement<CityType> cityElement = JAXB_PARSER.unmarshal(Resources.getResource("src/main/resources/city.xml").openStream());
+        CityType city = cityElement.getValue();
+        JAXBElement<CityType> cityElement2 =
+                new JAXBElement<>(new QName("archirand", "City"), CityType.class, city);
+        String strCity = JAXB_PARSER.marshal(cityElement2);
+        JAXB_PARSER.validate(strCity);
+        System.out.println(strCity);
+    }
+
+    @Test
+    public void test() throws Exception {
+        MainXML mainXML = new MainXML();
+        List<User> users = mainXML.getUsersByProject("topjava");
+        Assert.assertEquals(2, users.size());
+//        System.out.println(mainXML.toHtml(users, "topjava"));
+    }
+}
---
+++ web/upload/src/main/java/xml/schema/User.java
@@ -0,0 +1,156 @@
+
+package xml.schema;
+
+
+import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>Java class for anonymous complex type.
+ *
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ *
+ * <pre>
+ * &lt;complexType>
+ *   &lt;simpleContent>
+ *     &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema>string">
+ *       &lt;attribute name="flag" use="required" type="{archirand}flagType" />
+ *       &lt;attribute name="email" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *       &lt;attribute name="group" use="required" type="{http://www.w3.org/2001/XMLSchema}IDREFS" />
+ *       &lt;attribute name="city" type="{http://www.w3.org/2001/XMLSchema}IDREF" />
+ *     &lt;/extension>
+ *   &lt;/simpleContent>
+ * &lt;/complexType>
+ * </pre>
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+        "value"
+})
+@XmlRootElement(name = "User", namespace = "archirand")
+public class User {
+
+    @XmlValue
+    protected String value;
+    @XmlAttribute(name = "flag", required = true)
+    protected FlagType flag;
+    @XmlAttribute(name = "email", required = true)
+    protected String email;
+    @XmlAttribute(name = "group", required = true)
+    @XmlIDREF
+    @XmlSchemaType(name = "IDREFS")
+    protected List<Object> group;
+    @XmlAttribute(name = "city")
+    @XmlIDREF
+    @XmlSchemaType(name = "IDREF")
+    protected Object city;
+
+    /**
+     * Gets the value of the value property.
+     *
+     * @return possible object is
+     * {@link String }
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * Sets the value of the value property.
+     *
+     * @param value allowed object is
+     *              {@link String }
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    /**
+     * Gets the value of the flag property.
+     *
+     * @return possible object is
+     * {@link FlagType }
+     */
+    public FlagType getFlag() {
+        return flag;
+    }
+
+    /**
+     * Sets the value of the flag property.
+     *
+     * @param value allowed object is
+     *              {@link FlagType }
+     */
+    public void setFlag(FlagType value) {
+        this.flag = value;
+    }
+
+    /**
+     * Gets the value of the email property.
+     *
+     * @return possible object is
+     * {@link String }
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets the value of the email property.
+     *
+     * @param value allowed object is
+     *              {@link String }
+     */
+    public void setEmail(String value) {
+        this.email = value;
+    }
+
+    /**
+     * Gets the value of the group property.
+     *
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the group property.
+     *
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getGroup().add(newItem);
+     * </pre>
+     *
+     *
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Object }
+     */
+    public List<Object> getGroup() {
+        if (group == null) {
+            group = new ArrayList<Object>();
+        }
+        return this.group;
+    }
+
+    /**
+     * Gets the value of the city property.
+     *
+     * @return possible object is
+     * {@link Object }
+     */
+    public Object getCity() {
+        return city;
+    }
+
+    /**
+     * Sets the value of the city property.
+     *
+     * @param value allowed object is
+     *              {@link Object }
+     */
+    public void setCity(Object value) {
+        this.city = value;
+    }
+
+}
